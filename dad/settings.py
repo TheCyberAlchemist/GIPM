85b465b316b8c5df28ad162d913d40dac662bdab
Dev Patel <56962517+TheCyberAlchemist@users.noreply.github.com>
2021-10-07 11:00:26 +0530

    Added assembly model and changed the grn calculation with weight.

--- dad/settings.py
+++ dad/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+# pip install django-admin-volt
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -39,6 +39,7 @@ GRAPH_MODELS = {
 # Application definition
 
 INSTALLED_APPS = [
+    # 'admin_volt.apps.AdminVoltConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'ajax_datatable',
-    'django_extensions'
+    # 'django_extensions'
 ]
 
 MIDDLEWARE = [
